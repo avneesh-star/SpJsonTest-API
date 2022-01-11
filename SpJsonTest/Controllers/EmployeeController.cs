5c56c5e7177e28395a7eabd6ce04bbf06d51bf92
avneesh <avneeshgngwr@gmail.com>
2022-01-12 00:19:58 +0530

    Add project files.

---
+++ SpJsonTest/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpJsonTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SpJsonTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly TestdbContext _context;
+
+        public EmployeeController(TestdbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _context.Procedures.sp_get_employeeAsync();
+            var vales = JsonSerializer.Deserialize<Dictionary<string, object>>(data.FirstOrDefault().jsondata);
+            return Ok(vales);
+        }
+    }
+}
---
+++ SpJsonTest/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpJsonTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace SpJsonTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly TestdbContext _context;
+
+        public StudentController(TestdbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            var data = await _context.Procedures.sp_get_studentAsync();
+            var vales = JsonSerializer.Deserialize<Dictionary<string, object>>(data.FirstOrDefault().jsondata);
+            return Ok(vales);
+        }
+    }
+}
